1240414b942500e83013c0530f8a498e7dd69ee7
Daniel Hara <haradaniel734@gmail.com>
2021-05-15 17:22:59 +0200

    Very basic func

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
+import React, { useState } from 'react';
 import styles from './App.module.scss';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addToList = (todo) => {
+    setList(list.concat(todo))
+  }
+
   return (
     <div className={styles.App}>
-      Some stuff
+      <input
+        placeholder="Create a new todo..."
+        value={newTodo}
+        onChange={ (event) => {
+          setNewTodo(event.target.value);
+        } }
+        onKeyPress={event => {
+          if (event.key === 'Enter') {
+            addToList(newTodo);
+            setNewTodo('');
+          }
+        }}
+      />
+
+      <ul>
+        {list.map((item, index) => <li>
+            {item}
+          </li>
+        )}
+      </ul>
     </div>
   );
 }
